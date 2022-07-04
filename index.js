78d4fe0abe79605b6ab25af30d9e394cb4e57c91
EkaterinaShatalova <c.shatalova@mail.ru>
2022-07-04 17:10:09 +0300

    button added

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const inputCitiesFrom = document.querySelector(".input__cities-from");
 inputCitiesFrom.addEventListener("input", () => {
   console.log("событие инпут");
 });
+
+// const cities = JSON.parse(cities.json);
+
+// console.log(cities);
+
+// fetch("https://example.com/main.json")
+//   then(((response)) => response.json())
+//  .then(((json)) => {
+//   //console.log(json);
+//  });;
+
+
+
+const 
