60bdf75af544a5860627de4e8e9e945d9d276e9a
EkaterinaShatalova <c.shatalova@mail.ru>
2022-07-04 17:14:20 +0300

    depart added

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const inputCitiesFrom = document.querySelector(".input__cities-from");
 inputCitiesFrom.addEventListener("input", () => {
   console.log("событие инпут");
 });
+const inputCitiesTo = document.querySelector(".input__cities-to");
+
+inputCitiesTo.addEventListener("input", () => {
+  console.log("событие инпут");
+});
+const inputDepart = document.querySelector(".input__date-depart");
+
+inputDepart.addEventListener("input", () => {
+  console.log("событие инпут");
+});
 
 // const cities = JSON.parse(cities.json);
 
